35c1cba5718b384807660b3775e7fd9820976685
SANYAL <136782724+sanygit@users.noreply.github.com>
2023-08-15 06:05:08 -0700

    Add files via upload

---
+++ js/script-owl.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+})
+
